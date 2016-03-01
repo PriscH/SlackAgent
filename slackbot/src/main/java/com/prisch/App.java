faaaf1cd0788f86e71254d1ea8e6cd99bd29a8c2
Jaco Prinsloo <jmprinsloo@gmail.com>
2016-03-01 22:13:11 +0200

    Created the basic template of a Slack bot that can be deployed on OpenShift.

---
+++ slackbot/src/main/java/com/prisch/App.java
@@ -0,0 +1,45 @@
+package com.prisch;
+
+import com.ullink.slack.simpleslackapi.SlackMessageHandle;
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
+
+import java.io.IOException;
+import java.util.Random;
+
+public class App {
+
+    private static final String[] TASKS = {
+            "Help with documentation",
+            "Performance improvements on the reports",
+            "Troubleshoot the i-District interface",
+            "Deploy to DEV",
+            "Reintegrate the branch"
+    };
+
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        SlackSession session = SlackSessionFactory.createWebSocketSlackSession("xoxb-23800751190-BueL6xAziOVuZM0I5I7nyU1B");
+        session.addMessagePostedListener(new BasicSlackListener());
+        session.connect();
+
+        while (true) {
+            Thread.sleep(1000L);
+        }
+    }
+
+    private static final class BasicSlackListener implements SlackMessagePostedListener {
+
+        @Override
+        public void onEvent(SlackMessagePosted event, SlackSession session) {
+            String botId = session.sessionPersona().getId();
+
+            if (!event.getSender().isBot() && event.getMessageContent().contains(botId)) {
+                SlackMessageHandle handle = session.sendMessage(event.getChannel(), TASKS[RANDOM.nextInt(5)], null);
+            }
+        }
+    }
+}
