4706ee5720bbd0d5e8c049f0efbad43fb45a3c89
Jaco <jmprinsloo@gmail.com>
2017-01-03 22:40:57 +0200

    Migrated to SparkJava (from NanoHTTPD).

--- server/src/main/java/com/prisch/AgentServer.java
+++ server/src/main/java/com/prisch/AgentServer.java
@@ -7,22 +7,21 @@ import com.prisch.messages.MessageMapping;
 import com.prisch.messages.NoContentMessage;
 import com.prisch.oauth.OAuthTokenRequest;
 import com.ullink.slack.simpleslackapi.SlackSession;
-import fi.iki.elonen.NanoHTTPD;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class AgentServer extends RouterHTTPD {
+public class AgentServer  {
 
     private static final int REQUEST_TIMEOUT_SECONDS = 60;
     private static final int CONNECTION_LOST_MILLISECONDS = REQUEST_TIMEOUT_SECONDS * 1000 + 5000;
-    private static final String JSON_DATA_PARAM = "postData";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AgentServer.class);
     private static final Gson GSON = new Gson();
@@ -37,21 +36,22 @@ public class AgentServer extends RouterHTTPD {
     private long lastConnectionTime;
 
     public AgentServer(String address, int port, SlackSession slackSession) {
-        super(address, port);
         this.slackSession = slackSession;
+
+        Spark.ipAddress(address);
+        Spark.port(port);
     }
 
     // ===== Interface =====
 
     public void start() throws IOException {
-        addRoute("/command", Method.GET, this::serveCommand); // Long polling for commands
-        addRoute("/result", Method.POST, this::serveResult); // Responses to commands
-        addRoute("/connect", Method.POST, this::handleConnection); // Checking server availability
-        addRoute("/slack", Method.POST, this::handleSlackAction); // Slack buttons
-        addRoute("/slack/oauth", Method.GET, this::handleSlackOAuth); // Slack app authorization
+        Spark.get("/command", this::serveCommand, GSON::toJson); // Long polling for commands
+        Spark.post("/result", this::serveResult); // Responses to commands
+        Spark.post("/connect", this::handleConnection); // Checking server availability
+        Spark.post("/slack", this::handleSlackAction); // Slack buttons
+        Spark.get("/slack/oauth", this::handleSlackOAuth); // Slack app authorization
 
-        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
-        LOGGER.info("Server running on " + getHostname() + ":" + getListeningPort());
+        LOGGER.info("Server is running.");
     }
 
     // TODO: The messageQueue can grow unbounded if the futures are never resolved
@@ -74,84 +74,60 @@ public class AgentServer extends RouterHTTPD {
         return future;
     }
 
+    public boolean isConnected() {
+        return (System.currentTimeMillis() - lastConnectionTime < CONNECTION_LOST_MILLISECONDS);
+    }
+
     // ===== HTTP Responders =====
 
-    private Response serveCommand(IHTTPSession session) {
+    private Object serveCommand(Request request, Response response) {
         lastConnectionTime = System.currentTimeMillis();
 
         try {
             Message message = messageQueue.poll(REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS);
             if (message == null) {
-                return serveMessage(new NoContentMessage());
+                return new NoContentMessage();
             } else {
-                return serveMessage(message);
+                return message;
             }
         } catch (InterruptedException ex) {
             LOGGER.error(ex.getMessage(), ex);
-            return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, MIME_PLAINTEXT, "Server thread was interrupted.");
+            response.status(204);
+            return "Server thread was interrupted.";
         }
     }
 
-    private Response serveMessage(Message message) {
-        String messageJson = GSON.toJson(message);
-        return newFixedLengthResponse(messageJson);
-    }
-
-    private Response serveResult(IHTTPSession session) {
-        String messageJson;
-        try {
-            Map<String, String> requestParams = new HashMap<>();
-            session.parseBody(requestParams);
-            messageJson = requestParams.get(JSON_DATA_PARAM);
-        } catch (IOException | ResponseException ex) {
-            LOGGER.error(ex.getMessage(), ex);
-            return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, MIME_PLAINTEXT, "Unable to parse parameters.");
-        }
-
+    private String serveResult(Request request, Response response) {
+        String messageJson = request.body();
         Class<? extends Message> messageClass = MessageMapping.responseMappingFor(messageJson);
         Message message = GSON.fromJson(messageJson, messageClass);
 
         if (futureMap.containsKey(message.getId())) {
             futureMap.remove(message.getId()).complete(message);
         }
-
-        return newFixedLengthResponse("Successfully parsed result.");
+        return "Successfully parsed result.";
     }
 
-    private Response handleConnection(IHTTPSession session) {
+    private String handleConnection(Request request, Response response) {
         LOGGER.info("The client managed to establish a successful connection.");
-        return newFixedLengthResponse(null);
+        return "Success";
     }
 
-    private Response handleSlackAction(IHTTPSession session) {
-        return newFixedLengthResponse(null);
+    private String handleSlackAction(Request request, Response response) {
+        return "";
     }
 
-    private Response handleSlackOAuth(IHTTPSession session) {
+    private String handleSlackOAuth(Request request, Response response) {
         final String CODE = "code";
 
-        if (!session.getParameters().containsKey(CODE)) {
-            handleInvalidOAuthCode();
+        String tokenCode = request.queryParams(CODE);
+        if (tokenCode == null || tokenCode.trim().isEmpty()) {
+            LOGGER.error("Slack did not provide an authorization code.");
+            response.status(400);
+            return "Unable to parse parameters.";
         }
 
-        Optional<String> tokenCode = session.getParameters().get(CODE).stream().findAny();
-        if (!tokenCode.isPresent()) {
-            handleInvalidOAuthCode();
-        }
-
-        executorService.execute(new OAuthTokenRequest(slackSession, tokenCode.get()));
-
-        return newFixedLengthResponse(null);
-    }
-
-    private Response handleInvalidOAuthCode() {
-        LOGGER.error("Slack did not provide an authorization code.");
-        return newFixedLengthResponse(Response.Status.BAD_REQUEST, MIME_PLAINTEXT, "Unable to parse parameters.");
-    }
-
-    // === Helpers ===
-
-    public boolean isConnected() {
-        return (System.currentTimeMillis() - lastConnectionTime < CONNECTION_LOST_MILLISECONDS);
+        executorService.execute(new OAuthTokenRequest(slackSession, tokenCode));
+        return "";
     }
 }
